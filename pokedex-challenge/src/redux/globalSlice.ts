25ee6c0733ea73b1f2a33697f04fdfd0617763da
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-07-06 17:42:44 -0700

    New pokedex project created

---
+++ pokedex-challenge/src/redux/globalSlice.ts
@@ -0,0 +1,11 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {}
+
+export const globalSlice = createSlice({
+    name: "global",
+    initialState,
+    reducers: {}
+});
+
+export default globalSlice.reducer;
