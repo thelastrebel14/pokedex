67543d2af6f19423c839809838461a495ff4f61f
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-07-10 17:15:08 -0700

    Pokemon globalSlice.ts action updated for selected pokemon

--- pokedex-challenge/src/components/Pokemon.definitions.ts
+++ pokedex-challenge/src/components/Pokemon.definitions.ts
@@ -1,24 +1,28 @@
+export interface IPokemonElement {
+    name: string;
+    detailsUrl: string;
+}
+
 export interface IPokemon {
     name: string;
-    pokemonSprite: any;
+    pokemonSprite: string;
     pokemonDetails: IPokemonDetails;
 }
 
 export interface IPokemonDetails {
     pokemonType: string;
     pokemonId: string;
-    pokemonName: string;
     pokemonHeight: number;
     pokemonWeight: number;
     pokemonAbilities: Array<IAbilities>;
     pokemonStats: Array<IStats>;
 }
 
-interface IAbilities {
+export interface IAbilities {
     abilityName: string;
 }
 
-interface IStats {
+export interface IStats {
     statName: string;
     baseStat: number;
 }
--- pokedex-challenge/src/redux/globalSlice.ts
+++ pokedex-challenge/src/redux/globalSlice.ts
@@ -1,11 +1,59 @@
 import {createSlice} from "@reduxjs/toolkit";
+import { IAbilities, IPokemon, IPokemonElement, IStats } from "../components/Pokemon.definitions";
 
-const initialState = {}
+export interface IGlobalInitialState {
+    pokemonList: Array<IPokemonElement>,
+    selectedPokemon: IPokemon
+}
+
+const initialState: IGlobalInitialState = {
+    pokemonList: [],
+    selectedPokemon: {
+        name: '',
+        pokemonSprite: '',
+        pokemonDetails: {
+            pokemonType: '',
+            pokemonId: '',
+            pokemonHeight: 0,
+            pokemonWeight: 0,
+            pokemonAbilities: [],
+            pokemonStats: []
+        }
+    }
+}
 
 export const globalSlice = createSlice({
     name: "global",
     initialState,
-    reducers: {}
+    reducers: {
+        setPokemonList: (state: IGlobalInitialState, action) => {
+            const {pokemonList} = action.payload;
+            const pokemons: Array<IPokemonElement> = pokemonList.map((pokemon: any) => ({name: pokemon.name, detailsUrl: pokemon.url}))
+            state.pokemonList = pokemons;
+        },
+        updateSelectedPokemon: (state: IGlobalInitialState, action) => {
+            const {selectedPokemon} = action.payload;
+            const pokemonTypes = selectedPokemon.types.map((pokemonType: any) => (
+                {typeName: pokemonType.name}
+                ));
+            const pokemonAbilities: Array<IAbilities> = selectedPokemon.abilities.map((pokemonAbility: any) => ({abilityName: pokemonAbility.ability.name}))
+            const pokemonStats: Array<IStats> = selectedPokemon.stats.map((pokemonStat: any) => ({statName: pokemonStat.stat.name, baseStat: pokemonStat.base_stat}))
+            const updatedSelectedPokemon: IPokemon = {
+                name: selectedPokemon.name,
+                pokemonSprite: selectedPokemon.sprites.front_default,
+                pokemonDetails: {
+                    pokemonType: pokemonTypes,
+                    pokemonId: selectedPokemon.id,
+                    pokemonHeight: selectedPokemon.height,
+                    pokemonWeight: selectedPokemon.weight,
+                    pokemonAbilities: pokemonAbilities,
+                    pokemonStats: pokemonStats
+                }
+            };
+            state.selectedPokemon = updatedSelectedPokemon;
+        }
+    }
 });
 
+export const { setPokemonList, updateSelectedPokemon } = globalSlice.actions;
 export default globalSlice.reducer;
