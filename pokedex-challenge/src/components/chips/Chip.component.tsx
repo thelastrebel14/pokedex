6eb25c2fd0c30f962f990496d0dd6c7505f27943
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-07-11 22:33:34 -0700

    Chip.component.tsx collor pallete updated

--- pokedex-challenge/src/components/chips/Chip.component.tsx
+++ pokedex-challenge/src/components/chips/Chip.component.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Chip.css'
-import { IChipProps } from './Chip.definitions';
+import { IChipProps, CHIP_BACKGROUND_COLOR } from './Chip.definitions';
 
 const Chip = (props: IChipProps) => {
     const {label} = props;
+    
+
+    type ChipLabel = keyof typeof CHIP_BACKGROUND_COLOR;
+    const chipColorClass = CHIP_BACKGROUND_COLOR[label as ChipLabel] || 'grey';
+    console.log({chipColorClass});
     return (
-      <div className='chip'>
+      <div className='chip' style={{backgroundColor: chipColorClass}}>
         <span className='chip-label'>{label}</span>
       </div>
     );
