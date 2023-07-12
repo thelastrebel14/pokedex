c633781268c452567f973b798748e57d470809f2
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-07-11 21:36:46 -0700

    PokemonDetails.component.tsx code cleanup

--- pokedex-challenge/src/components/pokemon-details/PokemonDetails.component.tsx
+++ pokedex-challenge/src/components/pokemon-details/PokemonDetails.component.tsx
@@ -13,38 +13,6 @@ const PokemonDetails = () => {
   const pokemonStats = selectedPokemon.pokemonDetails.pokemonStats;
   const pokemonDescription: IPokemonDetails = selectedPokemon.pokemonDetails;
 
-  const getPokemonStats = (): JSX.Element => {
-    const pokemonStats = selectedPokemon.pokemonDetails.pokemonStats;
-    return (
-        <div className='pokemon-stats-container'>
-            <span className='details-title'>Stats</span>
-            <div>
-            {pokemonStats.map((pokemonStat: IStats) => {
-                return (
-                    <div key={pokemonStat.statName} className='stats-bar-container'>
-                        <span className='stats-bar-label'>{pokemonStat.statName}</span>
-                        <div>
-                            <div className="progress">
-                                <div
-                                className="progress-bar"
-                                role="progressbar"
-                                aria-valuenow={pokemonStat.baseStat}
-                                aria-valuemin={0}
-                                aria-valuemax={100}
-                                style={{width:`${pokemonStat.baseStat}%`}}
-                                >
-                                    <span className="sr-only">{pokemonStat.baseStat}</span>
-                                </div>
-                            </div>  
-                        </div>
-                    </div> 
-                )
-            })}
-            </div>
-        </div>
-    );
-  }
-
   const getPokemonTypes = (): JSX.Element => {
     const pokemonDescription: IPokemonDetails = selectedPokemon.pokemonDetails;
     const pokemonTypesLabelList = pokemonDescription.pokemonType.map((type: IType) => (type.typeName));
@@ -57,29 +25,8 @@ const PokemonDetails = () => {
     return getChipsList(pokemonAbilitiesLabelList, "Abilities");
   }
 
-  const getPokemonDescription = (): JSX.Element => {
-    const pokemonDescription: IPokemonDetails = selectedPokemon.pokemonDetails;
-    console.log({pokemonDescription});
-
-    return (
-        <div className='pokemon-details-description'>
-            <span>{selectedPokemon.name}</span>
-            <span>{pokemonDescription.pokemonId}</span>
-            <span>{pokemonDescription.pokemonHeight}</span>
-            <span>{pokemonDescription.pokemonWeight}</span>
-        </div>
-    );
-  }
-
     return (
       <div className='pokemon-details-container'>
-        {/* <div className='pokemon-description'>
-            {getPokemonDescription()}
-        </div>
-        <div className='pokemon-details'>
-            {getPokemonStats()}
-            {getPokemonAbilities()}
-        </div> */}
         <PokemonDescription
             pokemonName={selectedPokemon.name}
             pokemonId={pokemonDescription.pokemonId}
