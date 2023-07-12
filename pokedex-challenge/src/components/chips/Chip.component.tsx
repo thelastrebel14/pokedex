861cee14dd1a9a60f0cf8d4cde85f6d29b00a713
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-07-12 14:16:23 -0700

    chip cleanup

--- pokedex-challenge/src/components/chips/Chip.component.tsx
+++ pokedex-challenge/src/components/chips/Chip.component.tsx
@@ -5,10 +5,8 @@ import { IChipProps, CHIP_BACKGROUND_COLOR } from './Chip.definitions';
 const Chip = (props: IChipProps) => {
     const {label} = props;
     
-
     type ChipLabel = keyof typeof CHIP_BACKGROUND_COLOR;
     const chipColorClass = CHIP_BACKGROUND_COLOR[label as ChipLabel] || 'grey';
-    console.log({chipColorClass});
     return (
       <div className='chip' style={{backgroundColor: chipColorClass}}>
         <span className='chip-label'>{label}</span>
