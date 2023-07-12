38d4c2e346c35c53c3ee14bf34f1bb0170ee427d
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-07-11 19:49:43 -0700

    Bootstrap added to dependencies

--- pokedex-challenge/src/main.tsx
+++ pokedex-challenge/src/main.tsx
@@ -5,6 +5,9 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import {store} from './redux/store';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
 
 ReactDOM.render(
   <Provider store={store}>
