2a782cef70388ce7380f81da00b772c871d6e096
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-07-12 16:36:34 -0700

    Button component refactor for generic use

--- pokedex-challenge/src/components/button/Button.component.tsx
+++ pokedex-challenge/src/components/button/Button.component.tsx
@@ -1,39 +1,11 @@
 import React from 'react';
 import './Button.css'
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import { setPokemonList } from '../../redux/globalSlice';
+import { IButtonProps } from './Button.definitions';
 
-const Button = () => {
-    const dispatch = useDispatch();
-    const globalStore = useSelector((state: any) => state.global);
-    console.log({globalStore});
-    const getPreviousPokemonList = async () => {
-        try {
-          await axios.get(globalStore.previous)
-          .then((data) => {
-            dispatch(setPokemonList({pokemonList: data.data.results, next: data.data.next, previous: data.data.previous}))
-          });
-        } catch (error) {
-          console.error('Error:', error);
-        }
-      };
-
-    const getNextPokemonList = async () => {
-        try {
-          await axios.get(globalStore.next)
-          .then((data) => {
-            dispatch(setPokemonList({pokemonList: data.data.results, next: data.data.next, previous: data.data.previous}))
-          });
-        } catch (error) {
-          console.error('Error:', error);
-        }
-      };
+const Button = (props: IButtonProps) => {
+    const { onClick, label } = props;
     return (
-      <div className='pagination-container'>
-        {globalStore.previous && <button type="button" onClick={getPreviousPokemonList}>Previous</button>}
-        {globalStore.next && <button type="button" onClick={getNextPokemonList}>Next</button>}
-      </div>
+        <button type="button" className='button' onClick={onClick}>{label}</button>
     );
 }
 
